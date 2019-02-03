f19e3d4b2cb91c43c7cbcdc8f60820c4532053bb
Priti Kumari <priti.kumari2910@gmail.com>
2019-02-03 22:37:11 +0530

    My Project setup is done successfully

---
+++ shopping-backend/src/main/java/home_spring_priti/shopping_backend/App.java
@@ -0,0 +1,13 @@
+package home_spring_priti.shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
